1c9299f125b35ead385641c367408aa82f1caa3b
Candice Hall <hall.candice@outlook.com>
2022-03-22 22:10:39 -0400

    set up repo display and error handling

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,13 +1,68 @@
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+var repoContainerEl = document.querySelector("#repos-container");
+var repoSearchTerm = document.querySelector("#repo-search-term");
+
+var formSubmitHandler = function(event){
+event.preventDefault();
+var username = nameInputEl.value.trim();
+
+if (username) {
+    getUserRepos(username);
+    nameInputEl.value = "";
+} else {
+    alert("please enter a github name");
+}
+};
+
 var getUserRepos = function (user) {
     //format the github api url
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
 
     //make a request to the url
-    fetch(apiUrl).then(function (response) {
+    fetch(apiUrl)
+    .then(function (response) {
+        if (response.ok){
         response.json().then(function (data) {
-            console.log(data);
+           displayRepos(data, user);
         });
+    } else {
+        alert("Error: Github User Not Found");
+    }
+    })
+    .catch(function(error){
+        alert("Unable to connect to Github");
     });
 };
 
-getUserRepos("candiecane7");
+var displayRepos = function(repos, searchTerm){
+    if (repos.length === 0){
+        repoContainerEl.textContent = "No Repositories found.";
+    }
+    repoContainerEl.textContent = "";
+    repoSearchTerm.textContent = searchTerm;
+    for (var i = 0; i < repos.length; i++){
+        var repoName = repos[i].owner.login + "/" + repos[i].name;
+
+        var repoEl = document.createElement("div");
+        repoEl.classList = "list-item flex-row justify-space-between align-center";
+
+        var titleEl = document.createElement("span");
+        titleEl.textContent = repoName;
+
+        repoEl.appendChild(titleEl);
+
+        var statusEl = document.createElement("span");
+        statusEl.classList = "flex-row align-center";
+
+        if (repos[i].open_issues_count > 0){
+            statusEl.innerHTML = 
+            "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + " issue(s)";
+        } else {
+            statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
+        }
+        repoEl.appendChild(statusEl);
+        repoContainerEl.appendChild(repoEl);
+    }
+}
+userFormEl.addEventListener("submit", formSubmitHandler);
